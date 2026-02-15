27822ef51766b063fb3c2f23b3be94ac244213b1
sboslurm <borawhocodess@gmail.com>
2026-02-15 18:36:14 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tabICLsbo
+
+implementing [paper](https://arxiv.org/abs/2602.11139)
---
+++ train_tabiclsbo.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Config:
+    pass
+
+
+def set_seed(seed):
+    pass
+
+
+class TabICLv2:
+    def __init__(self, config=None):
+        pass
